17de5bde3f4e48c0f94bc41907c82b39d24c3c54
Pujan Parkh <pujan3892@gmail.com>
2016-11-29 15:50:34 -0800

    mod front end

--- front end/script/script.js
+++ front end/script/script.js
@@ -2,10 +2,22 @@ var typingTimer; //timer identifier
 var doneTypingInterval = 2000;
 var placeSearch, autocomplete;
 
+ var locationss = [
+      ['High risk', 37.3472753, -121.9046515, 20],
+      ['caution', 37.3394506, -121.8991359, 19],
+      ['caution', 37.3301157, -121.8990128, 19],
+      ['caution', 37.3313414, -121.896420, 19],
+
+    ];
+
+var marker, i;
+
 function initialize() {
     initMap();
     initAutoComplete();
 }
+
+
 
 function initAutoComplete() {
     autocomplete2 = new google.maps.places.Autocomplete(document.getElementById('end'));
@@ -24,6 +36,18 @@ function initMap() {
             lng: -121.882374
         },
     });
+
+for (i = 0; i < locationss.length; i++) { 
+        marker = new google.maps.Marker({
+        position: new google.maps.LatLng(locationss[i][1], locationss[i][2]),
+        map: map
+      });
+    }
+
+
+
+
+
     directionsDisplay.setMap(map);
     document.getElementById('submit').addEventListener('click', function() {
         calculateAndDisplayRoute(directionsService, directionsDisplay);
@@ -31,20 +55,11 @@ function initMap() {
 }
 
 function calculateAndDisplayRoute(directionsService, directionsDisplay) {
-    var waypts = [];
-    var checkboxArray = document.getElementById('waypoints');
-    for (var i = 0; i < checkboxArray.length; i++) {
-        if (checkboxArray.options[i].selected) {
-            waypts.push({
-                location: checkboxArray[i].value,
-                stopover: true
-            });
-        }
-    }
+
     directionsService.route({
         origin: document.getElementById('autocomplete').value,
         destination: document.getElementById('end').value,
-        waypoints: waypts,
+        //waypoints: waypts,
         optimizeWaypoints: true,
         travelMode: 'DRIVING'
     }, function(response, status) {
@@ -58,8 +73,8 @@ function calculateAndDisplayRoute(directionsService, directionsDisplay) {
                 var routeSegment = i + 1;
                 summaryPanel.innerHTML += '<b>Route Segment: ' + routeSegment + '</b><br>';
                 summaryPanel.innerHTML += route.legs[i].start_address + ' to ';
-                summaryPanel.innerHTML += route.legs[i].end_address + '<br>';
-                summaryPanel.innerHTML += route.legs[i].duration.text + '<br><br>';
+                summaryPanel.innerHTML += route.legs[i].end_address + '<br> Total time:';
+                summaryPanel.innerHTML += route.legs[i].duration.text + '<br><br> total distance:';
                 summaryPanel.innerHTML += route.legs[i].distance.text + '<br><br>';
                 console.log(route.legs[i]);
             }
